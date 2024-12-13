d6630e1bb6164bae726be3ec2dd7b4cc4f0d1c71
cherif <22014@esp.mr>
2024-12-13 12:01:28 +0100

    les rests

--- tp2note/src/main/java/fr/univtours/polytech/tp2note/rest/FilmRest.java
+++ tp2note/src/main/java/fr/univtours/polytech/tp2note/rest/FilmRest.java
@@ -1,5 +1,7 @@
 package fr.univtours.polytech.tp2note.rest;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import fr.univtours.polytech.tp2note.business.FilmBusiness;
@@ -9,11 +11,12 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 
 @Path("v1")
 public class FilmRest {
-    
+
     @EJB
     private FilmBusiness filmBusiness;
 
@@ -28,12 +31,29 @@ public class FilmRest {
     @GET
     @Path("films/{id}")
     @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
-    public FilmBean getLocation(@PathParam("id") Integer idFilm) {
+    public FilmBean getFilm(@PathParam("id") Integer idFilm) {
         FilmBean filmBean = this.filmBusiness.getFilm(idFilm);
         return filmBean;
     }
 
-   
-
+    @Path("films/filter")
+    @GET
+    @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+    public List<FilmBean> getFilms(@QueryParam("note") String filtreNote, @QueryParam("title") String filtreTitle) {
+        List<FilmBean> films = this.filmBusiness.getFilms();
+        System.err.println("title filter "+filtreTitle+" notefilter "+filtreNote+" nombre des films "+films.size());
+        
+        if (null != filtreTitle && !"".equals(filtreTitle)) {
+            // Un filtre est indiqué. On l'applique sur le champ "title".
+            films.removeIf(filmBean -> !filtreTitle.toLowerCase().equals(filmBean.getTitle().toLowerCase()));
+        } else if (null != filtreNote && !"".equals(filtreNote)) {
+            // Un filtre est indiqué. On l'applique sur le champ "note".
+            films.removeIf(filmBean -> !filtreNote.toLowerCase().equals(filmBean.getNote().toString().toLowerCase()));
+        }
     
+        return films;
+
 }
+}
+
+
