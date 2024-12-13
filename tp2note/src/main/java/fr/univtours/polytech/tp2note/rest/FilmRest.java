e3d7b9bdf619313b59ed1d43be7b940e485c3d59
cherif <22014@esp.mr>
2024-12-13 12:22:01 +0100

    batch put

--- tp2note/src/main/java/fr/univtours/polytech/tp2note/rest/FilmRest.java
+++ tp2note/src/main/java/fr/univtours/polytech/tp2note/rest/FilmRest.java
@@ -8,11 +8,15 @@ import fr.univtours.polytech.tp2note.business.FilmBusiness;
 import fr.univtours.polytech.tp2note.models.FilmBean;
 import jakarta.ejb.EJB;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.PATCH;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response.Status;
 
 @Path("v1")
 public class FilmRest {
@@ -41,8 +45,9 @@ public class FilmRest {
     @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
     public List<FilmBean> getFilms(@QueryParam("note") String filtreNote, @QueryParam("title") String filtreTitle) {
         List<FilmBean> films = this.filmBusiness.getFilms();
-        System.err.println("title filter "+filtreTitle+" notefilter "+filtreNote+" nombre des films "+films.size());
-        
+        System.err.println(
+                "title filter " + filtreTitle + " notefilter " + filtreNote + " nombre des films " + films.size());
+
         if (null != filtreTitle && !"".equals(filtreTitle)) {
             // Un filtre est indiqué. On l'applique sur le champ "title".
             films.removeIf(filmBean -> !filtreTitle.toLowerCase().equals(filmBean.getTitle().toLowerCase()));
@@ -50,10 +55,44 @@ public class FilmRest {
             // Un filtre est indiqué. On l'applique sur le champ "note".
             films.removeIf(filmBean -> !filtreNote.toLowerCase().equals(filmBean.getNote().toString().toLowerCase()));
         }
-    
+
         return films;
 
-}
-}
+    }
 
+    @PATCH
+    @Path("films/{id}")
+    public Response partialUpdateFilm(@PathParam("id") Integer idFilm, FilmBean filmBean) {
+        FilmBean odlFilmBean = this.filmBusiness.getFilm(idFilm);
+        if (null == odlFilmBean) {
+            return Response.status(Status.NOT_FOUND).build();
+        }
 
+        // Seul les champs renseignés dans le bean envoyé sont mis à jour en base de
+        // données.
+        if (null != filmBean.getNote() && !"".equals(filmBean.getNote())) {
+            odlFilmBean.setNote(filmBean.getNote());
+        }
+        if (null != filmBean.getTitle() && !"".equals(filmBean.getTitle())) {
+            odlFilmBean.setTitle(filmBean.getTitle());
+        }
+
+        this.filmBusiness.updateFilm(odlFilmBean);
+        return Response.status(Status.ACCEPTED).build();
+    }
+
+    @PUT
+    @Path("films/{id}")
+    public Response fullUpdateFilm(@PathParam("id") Integer idFilm, FilmBean filmBean) {
+        FilmBean odlFilmBean = this.filmBusiness.getFilm(idFilm);
+        if (null == odlFilmBean) {
+            return Response.status(Status.NOT_FOUND).build();
+        }
+
+        // On met à jour tous les champs :
+        odlFilmBean.setNote(filmBean.getNote());
+        odlFilmBean.setTitle(filmBean.getTitle());
+        this.filmBusiness.updateFilm(odlFilmBean);
+        return Response.status(Status.ACCEPTED).build();
+    }
+}
