4ae3bbb6a4cfec73cb54077dc8b9d4e9d65b4c8d
cherif <22014@esp.mr>
2024-12-13 08:43:32 +0100

    modelbean

--- tp2note/src/main/java/fr/univtours/polytech/tp2note/models/FilmBean.java
+++ tp2note/src/main/java/fr/univtours/polytech/tp2note/models/FilmBean.java
@@ -1,5 +1,23 @@
 package fr.univtours.polytech.tp2note.models;
 
-public class Film {
+import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.xml.bind.annotation.XmlRootElement;
+
+
+@Entity
+@XmlRootElement
+public class FilmBean implements Serializable {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private int note;
+    private String title;
+    
 }
